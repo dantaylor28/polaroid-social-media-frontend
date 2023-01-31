a25f121547c3c3b274cbf2ad5f1f98c089218f23
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-31 13:37:27 +0000

    Add links to navbar and remove href attributes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,20 +5,21 @@ const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Sign In</Nav.Link>
+            <Nav.Link>Create Account</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item>Action</NavDropdown.Item>
+              <NavDropdown.Item>
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item>Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item>
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
