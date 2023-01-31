6cc1bc3f132f99a08a97307b4e5a917cebce61c3
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-31 16:43:20 +0000

    Add routes for sign in and create account and adding padding-top to App.css

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
   return (
     <div className={styles.App}>
       <NavBar />
-      <Container>
+      <Container className={styles.HomePage}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route path="/create/account" render={() => <h1>Create Account</h1>} />
         </Switch>
       </Container>
     </div>
