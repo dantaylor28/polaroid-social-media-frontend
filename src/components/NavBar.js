797125420212592d3f52ed1195ad990445d46fe0
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-31 16:53:04 +0000

    Add NavLinks to the correct urls for my navbar buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,33 @@
 import React from "react";
-import styles from "../styles/NavBar.module.css"
+import styles from "../styles/NavBar.module.css";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar className={styles.NavBar} bg="dark" variant="dark" expand="md" fixed="top">
+    <Navbar
+      className={styles.NavBar}
+      bg="dark"
+      variant="dark"
+      expand="md"
+      fixed="top"
+    >
       <Container>
-        <Navbar.Brand>Logo here</Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>Logo here</Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Sign In</Nav.Link>
-            <Nav.Link>Create Account</Nav.Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/signin">Sign In</NavLink>
+            <NavLink to="/create/account">Create Account</NavLink>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item>Action</NavDropdown.Item>
-              <NavDropdown.Item>
-                Another action
-              </NavDropdown.Item>
+              <NavDropdown.Item>Another action</NavDropdown.Item>
               <NavDropdown.Item>Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item>
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item>Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
