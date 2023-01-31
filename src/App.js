31114830807c077d7739d1c5a3e07d2207c1fd3f
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-31 16:33:55 +0000

    Add route for home in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
