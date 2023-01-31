9babc2922a37be6a6b96d8506fcc085d9583b766
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-31 12:08:03 +0000

    Install react bootstrap, add css link to index.html and ensure it works as expected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import "./App.css";
+import { Badge } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
+      <h1>
+        !!!!<Badge variant="secondary">New</Badge>
+      </h1>
       <p>Hello, world</p>
     </div>
   );
