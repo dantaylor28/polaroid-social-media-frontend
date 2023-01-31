9acabd1a96d06204c53fb8910fb59ca649f5b710
DanTaylor28 <dan.taylor1493@gmail.com>
2023-01-31 19:48:54 +0000

    Create skeleton layout of sign up form

--- src/pages/auth/CreateAccountForm.js
+++ src/pages/auth/CreateAccountForm.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const CreateAccountForm = () => {
   return (
-    <div>CreateAccountForm</div>
-  )
-}
+    <Row>
+      <Col md={6}>
+        <Container>
+          <h1>Sign up</h1>
+          <p>Sign up form here</p>
+        </Container>
+      </Col>
+      <Col md={6}>
+        <h1>Image here? maybe 2</h1>
+      </Col>
+    </Row>
+  );
+};
 
-export default CreateAccountForm
+export default CreateAccountForm;
