0a2fc9e40da7735558d8e2813a6ae2a42a080451
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-01 20:12:42 +0000

    Paste in a react bootstrap form template to CreateAccountForm.js

--- src/pages/auth/CreateAccountForm.js
+++ src/pages/auth/CreateAccountForm.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import styles from "../../styles/CreateAccountForm.module.css"
+import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import styles from "../../styles/CreateAccountForm.module.css";
 
 const CreateAccountForm = () => {
   return (
@@ -8,7 +8,26 @@ const CreateAccountForm = () => {
       <Col md={6}>
         <Container className={styles.Form}>
           <h1 className={styles.Heading}>create an account</h1>
-          <p>Sign up form here</p>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Container>
       </Col>
       <Col md={6}>
