ea0b99c997b3b19ccc450ca7fdda5af30ad6f11c
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-03 08:38:52 +0000

    Create SignInForm.module.css and add layout and basic color styles to form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import styles from "../../styles/SignInForm.module.css"
 
 const SignInForm = () => {
   return (
-    <Row>
-      <Col>
-        <Container>
-          <h1>sign into your account</h1>
+    <Row className={styles.Row}>
+      <Col className="my-auto" md={6}>
+        <Container className={styles.Form}>
+          <h1 className={styles.Heading}>sign into your account</h1>
           form here
         </Container>
       </Col>
