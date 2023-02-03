df09d0d1fed9ad5fca57c2d81720a20db2f64824
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-03 08:42:26 +0000

    Paste in basic login form from react bootstrap

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import styles from "../../styles/SignInForm.module.css"
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import styles from "../../styles/SignInForm.module.css";
 
 const SignInForm = () => {
   return (
@@ -8,7 +8,19 @@ const SignInForm = () => {
       <Col className="my-auto" md={6}>
         <Container className={styles.Form}>
           <h1 className={styles.Heading}>sign into your account</h1>
-          form here
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Container>
       </Col>
       <Col>image here</Col>
