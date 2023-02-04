b36a928f344096126bafaf7d17ef29ffcaed9a2f
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-04 18:07:10 +0000

    Add styling and correct fields to my CreatePostForm

--- src/pages/posts/CreatePostForm.js
+++ src/pages/posts/CreatePostForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Form, Row, Col } from "react-bootstrap";
 import styles from "../../styles/CreateEditPostForm.module.css";
+import formStyles from "../../styles/CreateAccountForm.module.css";
 
 function CreatePostForm() {
   return (
@@ -8,21 +9,37 @@ function CreatePostForm() {
       <Row>
         <Col md={5} lg={5}>
           <Container className={styles.CreatePostForm}>
-            <Form.Group>
-                <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" name="title" />
+            <h1 className={styles.Heading}>upload a post</h1>
+            <Form.Group className={formStyles.FormFields}>
+              <Form.Label className="d-none">Title</Form.Label>
+              <Form.Control
+                className={formStyles.FormTextBox}
+                type="text"
+                name="title"
+                placeholder="Title"
+              />
             </Form.Group>
-            <Form.Group>
-                <Form.Label>Caption</Form.Label>
-                    <Form.Control as="textarea" name="caption" rows={6} />
+            <Form.Group className={formStyles.FormFields}>
+              <Form.Label className="d-none">Caption</Form.Label>
+              <Form.Control
+                className={formStyles.FormTextBox}
+                as="textarea"
+                name="caption"
+                rows={6}
+                placeholder="Caption"
+              />
             </Form.Group>
-            <Form.Group>
-                <Form.Label>Category</Form.Label>
-                    <Form.Control as="select" custom name="category">
-                        <option value="general">general</option>
-                        <option value="landscapes">landscapes</option>
-                        <option value="portraits">portraits</option>
-                    </Form.Control>
+            <Form.Group className={`${formStyles.FormTextBox}`}>
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                as="select"
+                custom
+                name="category"
+              >
+                <option value="general">general</option>
+                <option value="landscapes">landscapes</option>
+                <option value="portraits">portraits</option>
+              </Form.Control>
             </Form.Group>
           </Container>
         </Col>
