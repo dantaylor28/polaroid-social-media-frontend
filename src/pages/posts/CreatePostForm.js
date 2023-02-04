315b1407e79ce71f3e6fc36f7ce3d45c25189f55
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-04 16:34:32 +0000

    Add skeleton layout for CreatePostForm.js

--- src/pages/posts/CreatePostForm.js
+++ src/pages/posts/CreatePostForm.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Container, Form, Row, Col } from "react-bootstrap";
 
 function CreatePostForm() {
   return (
-    <div>CreatePostForm</div>
-  )
+    <Form>
+      <Row>
+        <Col>
+          <Container>Upload post form here..</Container>
+        </Col>
+        <Col>
+          <Container>
+            Image preview here..
+          </Container>
+        </Col>
+      </Row>
+    </Form>
+  );
 }
 
-export default CreatePostForm
+export default CreatePostForm;
