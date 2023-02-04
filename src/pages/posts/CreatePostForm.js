3194ddd1e00f42792371729dd7ccc0c67286fdd6
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-04 16:53:18 +0000

    add a border and center the text in the image preview column

--- src/pages/posts/CreatePostForm.js
+++ src/pages/posts/CreatePostForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Form, Row, Col } from "react-bootstrap";
+import styles from "../../styles/CreateEditPostForm.module.css";
 
 function CreatePostForm() {
   return (
@@ -9,8 +10,12 @@ function CreatePostForm() {
           <Container>Upload post form here..</Container>
         </Col>
         <Col>
-          <Container>
-            Image preview here..
+          <Container className={`${styles.ImagePreview} d-flex flex-column justify-content-center`}>
+            <Form.Group className="text-center">
+                <Form.Label className="d-flex justify-content-center">
+                Image preview here..
+                </Form.Label>
+            </Form.Group>
           </Container>
         </Col>
       </Row>
