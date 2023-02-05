2318dd79f7ac8c0ec7f75865c23b1301c0ca87e8
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-05 17:47:07 +0000

    Make axiosReq request for a specific post and console log the details of it

--- src/pages/posts/PostDetail.js
+++ src/pages/posts/PostDetail.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostDetail() {
-  const { id } = useParams;
+  const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
       } catch (error) {
-        
+        console.log(error);
       }
-    }
-  })
+    };
+    handleMount();
+  }, [id]);
 
   return <div>PostDetail page to display a specified user post!</div>;
 }
