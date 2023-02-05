7be7c06018f811c6e2c4ddee875b470db4ef8265
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-05 16:34:22 +0000

    import the useEffect hook and define the handleMount function inside with a try/catch block

--- src/pages/posts/PostDetail.js
+++ src/pages/posts/PostDetail.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 function PostDetail() {
   const { id } = useParams;
   const [post, setPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+  })
 
   return <div>PostDetail page to display a specified user post!</div>;
 }
