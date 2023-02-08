532353b0cdb9270e14eddfdb4213d9c6b04fc9e4
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-08 10:45:18 +0000

    Add skeleton layout to PostList.js

--- src/pages/posts/PostList.js
+++ src/pages/posts/PostList.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function PostList() {
   return (
-    <div>PostList home page etc</div>
-  )
+    <Row>
+      <Col className="d-none d-lg-block p-0 p-lg-2" md={3}>
+        <p>Most followed profiles - desktop</p>
+      </Col>
+      <Col className="py-2 p-0 p-lg-2" lg={6}>
+        <p>My following profiles - mobile</p>
+        <Container>Post component here</Container>
+      </Col>
+      <Col className="d-none d-lg-block p-0 p-lg-2" md={3}>
+        <p>My following profiles - desktop</p>
+      </Col>
+    </Row>
+  );
 }
 
-export default PostList
+export default PostList;
