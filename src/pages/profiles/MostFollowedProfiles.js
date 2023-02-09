6f0b7e6a1a626f6abf8b196420bd4ec97561916d
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-09 21:05:29 +0000

    define the handleMount and axiosReq.get request inside useEffect hook

--- src/pages/profiles/MostFollowedProfiles.js
+++ src/pages/profiles/MostFollowedProfiles.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const MostFollowedProfiles = () => {
@@ -8,6 +9,23 @@ const MostFollowedProfiles = () => {
   });
   const { mostFollowedProfiles } = profileData;
   const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-num_of_followers"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          mostFollowedProfiles: data,
+        }));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleMount();
+  }, [currentUser]);
 
   return (
     <Container>
