e825a48f1f1b8940e772f3ea8eab0f6d48b96168
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-09 18:10:09 +0000

    Create EditCommentForm.js, paste in original CreateCommentForm and update the return statement

---
+++ src/pages/comments/EditCommentForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ProfileAvatar from "../../components/ProfileAvatar";
+import styles from "../../styles/CreateEditCommentForm.module.css";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function CreateCommentForm(props) {
+  const { post, setPost, setComments, profile_id, profileImage } = props;
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/comments/", {
+        text,
+        post,
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: [data, ...prevComments.results],
+      }));
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            num_of_comments: prevPost.results[0].num_of_comments + 1,
+          },
+        ],
+      }));
+      setText("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Form className={styles.Form} onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Control
+          className={styles.CommentForm}
+          as="textarea"
+          rows={1}
+          value={text}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <div className={styles.BtnDiv}>
+        <Button className={`${styles.Button} mb-3 mr-5`} type="button">
+          Cancel
+        </Button>
+        <Button className={`${styles.Button} mb-3 mr-5`} type="submit">
+          Update
+        </Button>
+      </div>
+    </Form>
+  );
+}
+
+export default CreateCommentForm;
