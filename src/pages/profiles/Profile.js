2b62939de6ab5093a567caa3ba20c03275cdec80
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-09 21:48:43 +0000

    Detsructure all of my props and declare variables in Profile.js page

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
-const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+const Profile = (props) => {
+  const { mobile, profile, avatarSize = 55 } = props;
+  const { id, following_id, profile_image, owner } = profile;
+  const currentUser = useCurrentUser()
+  const is_profile_owner = currentUser?.username === owner
+  return <div>Profile</div>;
+};
 
-export default Profile
+export default Profile;
