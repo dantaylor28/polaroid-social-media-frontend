4fa039ed301e2b3e9fc1de080845e4561d07848e
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-09 22:11:40 +0000

    Add ternary statement to Profile.js to shiw follow or unfollow button to users depending on certain conditions

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import ProfileAvatar from "../../components/ProfileAvatar";
+import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
   const { mobile, profile, avatarSize = 55 } = props;
   const { id, following_id, profile_image, owner } = profile;
-  const currentUser = useCurrentUser()
-  const is_profile_owner = currentUser?.username === owner
-  return <div>Profile</div>;
+  const currentUser = useCurrentUser();
+  const is_profile_owner = currentUser?.username === owner;
+
+  return (
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <ProfileAvatar src={profile_image} size={avatarSize} />
+          <span>{owner}</span>
+        </Link>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_profile_owner &&
+          (following_id ? (
+            <Button onClick={() => {}}>unfollow</Button>
+          ) : (
+            <Button onClick={() => {}}>follow</Button>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
