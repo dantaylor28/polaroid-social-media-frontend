824806b71c914d4cdb5b5e9882e74527f0af2573
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-09 20:52:01 +0000

    Create profiles folder and MostFollowedProfiles.js file

---
+++ src/pages/profiles/MostFollowedProfiles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const MostFollowedProfiles = () => {
+  return (
+    <Container>
+        <p>Check out the most followed profiles!</p>
+    </Container>
+  )
+}
+
+export default MostFollowedProfiles
