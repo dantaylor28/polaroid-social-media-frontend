e8350048a19daa6c14a706c44aa1a4ec64862b9e
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-09 13:37:45 +0000

    Get basic comment form dispalying in PostDetail.js

--- src/pages/comments/CreateCommentForm.js
+++ src/pages/comments/CreateCommentForm.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Form, FormControl, InputGroup } from "react-bootstrap";
+import { Form, InputGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ProfileAvatar from "../../components/ProfileAvatar";
 
 function CreateCommentForm(props) {
   const { post, setPost, setComments, profile_id, profileImage } = props;
+  
   return (
     <Form>
       <Form.Group>
         <InputGroup>
-          <FormControl
+          <Link to={`/profiles/${profile_id}`}>
+            <ProfileAvatar src={profileImage} size={55} />
+          </Link>
+          <Form.Control
             placeholder="comment here"
-            as="text-area"
+            as="textarea"
             rows={3}
             onChange={() => {}}
           />
--- src/pages/posts/PostDetail.js
+++ src/pages/posts/PostDetail.js
@@ -11,8 +11,8 @@ function PostDetail() {
   const [post, setPost] = useState({ results: [] });
 
   const currentUser = useCurrentUser();
-  const [comments, setComments] = useState({ results: [] });
   const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
