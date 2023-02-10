c9915c5b2cecba48eb1bdbd263477adbbafae0b1
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-10 18:40:01 +0000

    Update return statement in passwordForm to correct form layout

--- src/pages/profiles/PasswordForm.js
+++ src/pages/profiles/PasswordForm.js
@@ -7,15 +7,31 @@ const PasswordForm = () => {
         <Container>
           <Form onSubmit={handleSubmit}>
             <Form.Group>
-              <Form.Label>Update Username</Form.Label>
+              <Form.Label>Update Password</Form.Label>
               <Form.Control
-                type="text"
-                value={username}
-                placeholder="Username"
-                onChange={(event) => setUsername(event.target.value)}
+                type="password"
+                value={new_password1}
+                placeholder="Update Password"
+                name="new_password1"
+                onChange={handleChange}
               />
             </Form.Group>
-            {errors?.username?.map((message, idx) => (
+            {errors?.new_password1?.map((message, idx) => (
+              <Alert key={idx} variant="danger">
+                {message}
+              </Alert>
+            ))}
+            <Form.Group>
+              <Form.Label>Confirm New Password</Form.Label>
+              <Form.Control
+                type="password"
+                value={new_password2}
+                placeholder="Confirm New Password"
+                name="new_password2"
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors?.new_password2?.map((message, idx) => (
               <Alert key={idx} variant="danger">
                 {message}
               </Alert>
