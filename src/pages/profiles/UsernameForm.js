7a7a888ced2638818b7b19e77d56b3ddbd36e55e
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-10 18:14:54 +0000

    Define handle submit function

--- src/pages/profiles/UsernameForm.js
+++ src/pages/profiles/UsernameForm.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import { useCurrentUser, useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosRes } from "../../api/axiosDefaults";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
 
 const UsernameForm = () => {
   const [username, setUsername] = useState("");
@@ -13,13 +17,28 @@ const UsernameForm = () => {
 
   useEffect(() => {
     if (currentUser?.profile_id?.toString() === id) {
-        setUsername(currentUser.username)
+      setUsername(currentUser.username);
     } else {
-        history.push('/')
+      history.push("/");
     }
-  }, [currentUser, history, id])
+  }, [currentUser, history, id]);
 
-  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put("dj-rest-auth/user/", {
+        username,
+      });
+      setCurrentUser((prevUser) => ({
+        ...prevUser,
+        username,
+      }));
+      history.goBack();
+    } catch (error) {
+      console.log(error);
+      setErrors(error.response?.data);
+    }
+  };
 
   return <div>UsernameForm</div>;
 };
