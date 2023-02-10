b5c1693c1e8fc0c4dcc37f220c8a4f0a5e332285
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-10 09:51:12 +0000

    Add skeleton layout for profile page

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import MostFollowedProfiles from "./MostFollowedProfiles";
 
 function ProfilePage() {
+  const profileContent = (
+    <>
+      <Row>
+        <Col lg={4}>
+            <p>Image here</p>
+            <p>username here</p>
+        </Col>
+        <Col lg={8}>
+            <p>User stats here</p>
+            <p>follow button here</p>
+        </Col>
+        <Col className="p-3 text-center">bio here</Col>
+      </Row>
+    </>
+  );
+
   return (
-    <div>ProfilePage is going here</div>
-  )
+    <Row>
+      <Col lg={9}>
+        <MostFollowedProfiles mobile />
+        <Container>
+            {profileContent}
+        </Container>
+      </Col>
+      <Col lg={3} className="d-none d-lg-block p-0 p-lg-2">
+        <MostFollowedProfiles />
+      </Col>
+    </Row>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
