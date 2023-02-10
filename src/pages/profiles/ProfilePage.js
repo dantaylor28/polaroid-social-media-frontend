44461a759dd705de580d290c1fa1dbfefd03f669
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-10 10:02:22 +0000

    Destructure profileloaded abd setProfileLoaded and currentUser in profilepage.js

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import MostFollowedProfiles from "./MostFollowedProfiles";
 
 function ProfilePage() {
+  const [profileLoaded, setProfileLoaded] = useState(false);
+  const currentUser = useCurrentUser()
+
+  useEffect(() => {
+    setProfileLoaded(true)
+  }, [])
+
   const profileContent = (
     <>
-      <Row>
-        <Col lg={4}>
-            <p>Image here</p>
-            <p>username here</p>
+      <Row className="px-3 text-center">
+        <Col lg={4} className="text-lg-left">
+          <p>Image here</p>
+          <p>username here</p>
         </Col>
         <Col lg={8}>
-            <p>User stats here</p>
-            <p>follow button here</p>
+          <p>User stats here</p>
+          <p>follow button here</p>
         </Col>
         <Col className="p-3 text-center">bio here</Col>
       </Row>
@@ -21,13 +30,13 @@ function ProfilePage() {
 
   return (
     <Row>
-      <Col lg={9}>
+      <Col lg={8}>
         <MostFollowedProfiles mobile />
         <Container>
-            {profileContent}
+          {profileLoaded ? <>{profileContent}</> : <Asset spinner />}
         </Container>
       </Col>
-      <Col lg={3} className="d-none d-lg-block p-0 p-lg-2">
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         <MostFollowedProfiles />
       </Col>
     </Row>
