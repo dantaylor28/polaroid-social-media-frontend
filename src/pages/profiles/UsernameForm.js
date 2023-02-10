d5856cf84275053acf715f720e65aaf166f876be
DanTaylor28 <dan.taylor1493@gmail.com>
2023-02-10 18:11:12 +0000

    Add and use useEffect hook

--- src/pages/profiles/UsernameForm.js
+++ src/pages/profiles/UsernameForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
@@ -10,6 +10,16 @@ const UsernameForm = () => {
   const { id } = useParams();
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() === id) {
+        setUsername(currentUser.username)
+    } else {
+        history.push('/')
+    }
+  }, [currentUser, history, id])
+
+  
 
   return <div>UsernameForm</div>;
 };
