ab96bc06351b4ad0047b1e951f2e1681df4dd7d7
DanTaylor28 <dan.taylor1493@gmail.com>
2023-03-20 19:58:26 +0000

    Create PageNotFound.module.css and add style to pageNotFound.js

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Asset from "../components/Asset"
 import NoResults from "../assets/no-results.png"
+import styles from "../styles/PageNotFound.module.css"
 
 const PageNotFound = () => {
   return (
-    <div>
+    <div className={styles.Asset}>
         <Asset src={NoResults} message="Oops.. This page doesn't seem to exist!" />
     </div>
   )
