588a9b5c96254f6f8f3978659e9e4c284a438fac
DanTaylor28 <dan.taylor1493@gmail.com>
2023-03-28 20:04:57 +0000

    Create test ensuring sign in and sign up links render making it first fail and then pass as expected

--- src/components/__tests__/NavBar.test.js
+++ src/components/__tests__/NavBar.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
 import NavBar from "../NavBar";
@@ -23,7 +23,25 @@ test("Renders link to user profile", async () => {
     </Router>
   );
 
-  const ProfileLink = await screen.findByText("Profile")
-  expect(ProfileLink).toBeInTheDocument()
-  
+  const ProfileLink = await screen.findByText("Profile");
+  expect(ProfileLink).toBeInTheDocument();
+});
+
+test("Renders Sign In & Create Account links upon sign out", async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
+
+  const SignOutLink = await screen.findByRole("link", { name: "Sign Out" });
+  fireEvent.click(SignOutLink);
+
+  const SignInLink = await screen.findByText("Sign In");
+  const CreateAccountLink = await screen.findByText("Create Account");
+
+  expect(SignInLink).toBeInTheDocument();
+  expect(CreateAccountLink).toBeInTheDocument();
 });
